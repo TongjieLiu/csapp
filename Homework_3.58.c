1de6b92ce4378b698cb513a3537a9778d8157dd1
Tongjie Liu <tongjieandliu@gmail.com>
2021-05-05 14:37:22 +0800

    Add homework problem 3.58

---
+++ Homework_3.58.c
@@ -0,0 +1,23 @@
+/* A homework problem solution for the CS:APP3e
+
+   Copyright (C) 2021 Tongjie Liu <tongjieandliu@gmail.com>.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.*/
+
+long decode2(long x, long y, long z)
+{
+	y = y - z;
+	x = x * y;
+	return ((y << 63) >> 63) ^ x;
+}
